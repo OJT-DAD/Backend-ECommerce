d65a082d3cd5e873505e9204ef37e7e4e418cafc
atikartyas <58196244+atikartyas@users.noreply.github.com>
2021-05-19 21:33:06 +0700

    Update#024
    Update controller

---
+++ OnlineShop/OnlineShop/Controllers/AllRoles/HistoryController.cs
@@ -0,0 +1,31 @@
+﻿using Application.AllRoles.Histories.Queries.GetAllHistory;
+using Application.AllRoles.Histories.Queries.GetAllStoreHistory;
+using Application.AllRoles.Histories.Queries.GetAllUserHistory;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineShop.Controllers.AllRoles
+{
+    [Route("history")]
+    public class HistoryController : ApiControllerBase
+    {
+        [HttpGet("user-history/{userId}")]
+        public async Task<GetAllUserHistoryVm> GetUserHistory(int userId)
+        {
+            return await Mediator.Send(new GetAllUserHistoryQuery { UserId = userId });
+        }
+        [HttpGet("seller-history/{storeId}")]
+        public async Task<GetAllStoreHistoryVm> GetStoreHistory(int storeId)
+        {
+            return await Mediator.Send(new GetAllStoreHistoryQuery { StoreId = storeId });
+        }
+        [HttpGet("admin-history")]
+        public async Task<GetAllHistoryVm> GetAdminHistory()
+        {
+            return await Mediator.Send(new GetAllHistoryQuery());
+        }
+    }
+}
